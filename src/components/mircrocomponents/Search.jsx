b6fd6a6f660a84a6b9cb3b83ef1256b005afae07
kalungi Rasuli <kalungirasuli495@gmail.com>
2024-01-20 07:36:42 -0800

    corrected search breack down

--- src/components/mircrocomponents/Search.jsx
+++ src/components/mircrocomponents/Search.jsx
@@ -19,7 +19,7 @@ export default function Search(){
     return(
         <>
         <div className="bg-slate-400 p-2 py-4 border-none w-[80%] h-[114px] relative m-auto mt-5 mb-5 md:w-[50%]">
-            <p className=" text-[20px] text-center pb-2 text-stone-900  font-mono md:text-left pl-4"> Search a snippet in {"Git"}</p>
+            <p className=" text-[15px] text-center pb-2 text-stone-900  font-mono md:text-left pl-4 md:text-[20px]"> Search a snippet in {"Git"}</p>
             <div className=" flex flex-row w-[100%] m-auto ">
                 <input type="text" className="w-[90%] py-2 pl-4 focus:border-slate-600 border-solid border-2 text-slate-500 font-mono" />
                 <span onClick={setter} className="p-2 w-[10%]  block justify-center mt-1 absolute right-0 bg-slate-600 focus:bg-white md:w-[32px]" title="Select from dropdowm">{<GoTriangleDown style={style}/>}</span>
