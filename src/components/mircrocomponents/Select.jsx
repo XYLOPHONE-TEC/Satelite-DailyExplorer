747f0b9152c615a18b761000fa8c67d9fba47db1
kalungi Rasuli <kalungirasuli495@gmail.com>
2024-01-20 07:42:32 -0800

    roundedconrers removed on select menus

--- src/components/mircrocomponents/Select.jsx
+++ src/components/mircrocomponents/Select.jsx
@@ -2,7 +2,7 @@ export default function Select(){
     return(
         <>
         <div className="select bg-slate-300 p-2 py-4 border-none w-[80%]   m-auto mt-5 mb-5 md:w-[50%] flex justify-center">
-            <select name="select" className="w-[90%] py-2 pl-4 bg-white  focus:border-slate-600 border-solid border-2 text-slate-500 font-mono">
+            <select name="select" className="w-[90%] py-3 rounded-none pl-4 bg-white   focus:border-slate-600 border-solid border-2 text-slate-500 font-mono">
                 <option value="" className="">Value 1</option>
                 <option value="" className="">Value 2</option>
                 <option value="" className="">Value 3</option>
